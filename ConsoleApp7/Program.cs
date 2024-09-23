a450003978d219fb9399a814140d54b81d9d6c0b
Natalia <natabilcuk@gmail.com>
2024-09-23 15:49:03 +0300

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace HW_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+
+            try
+            {
+                int number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"You entered a number: {number}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("It's not a number!");
+            }
+
+        }
+    }
+}
